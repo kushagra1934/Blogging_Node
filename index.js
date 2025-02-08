9d789942754fe1cef3f7b8051c926a9b8a58475f
Kushagra Saxena <kushagra.2022ug1093@iiitranchi.ac.in>
2025-02-09 00:33:42 +0530

    add blog

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const {
   checkForAuthenticationCookie,
 } = require("./middlewares/authentication");
 
+const blogRoute = require("./routes/blog");
+
 const app = express();
 const PORT = 8000;
 
@@ -35,6 +37,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
 app.listen(PORT, () => {
   console.log(`Server started at port: ${PORT}`);
